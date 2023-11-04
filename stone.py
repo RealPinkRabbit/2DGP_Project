b79cec0281255435c102a6d87abd450aacf9a66f
RealPinkRabbit <sdg0896@gmail.com>
2023-11-04 17:49:07 +0900

    벽 충돌 코드부분 함수화

--- stone.py
+++ stone.py
@@ -7,13 +7,16 @@ x_max_boundary = 1280
 y_min_boundary = 0
 y_max_boundary = 800
 
+
 class blue_stone:
+
     image = None
     minV = 0.02
     vDecRate = 0.98
+
     def __init__(self, x = 600, y = 500):
         self.x, self.y = x, y
-        self.vx, self.vy = 300, -300
+        self.vx, self.vy = 100, -100
         self.radius = 32
         if (blue_stone.image == None):
             blue_stone.image = load_image('Stone_Blue_64x64.png')
@@ -23,23 +26,29 @@ class blue_stone:
 
     def update(self):
         self.x += self.vx
+        self.y += self.vy
+
+        self.stone_wall_collision_check()
+
+        self.vx *= blue_stone.vDecRate
+        if (fabs(self.vx) < blue_stone.minV):
+            self.vx = 0
+
+        self.vy *= blue_stone.vDecRate
+        if (fabs(self.vx) < blue_stone.minV):
+            self.vy = 0
+
+    def stone_wall_collision_check(self):
         if (self.x < x_min_boundary + self.radius):
             self.x += 2*(self.radius - (self.x - x_min_boundary))
             self.vx *= -1
         elif (self.x > x_max_boundary - self.radius):
             self.x -= 2*(self.x - (x_max_boundary - self.radius))
             self.vx *= -1
-        self.vx *= blue_stone.vDecRate
-        if (fabs(self.vx) < blue_stone.minV):
-            self.vx = 0
 
-        self.y += self.vy
         if (self.y < y_min_boundary + self.radius):
             self.y += 2*(self.radius - (self.y - y_min_boundary))
             self.vy *= -1
         elif (self.y > y_max_boundary - self.radius):
             self.y -= 2*(self.y - (y_max_boundary - self.radius))
             self.vy *= -1
-        self.vy *= blue_stone.vDecRate
-        if (fabs(self.vx) < blue_stone.minV):
-            self.vy = 0
