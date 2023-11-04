bff12a0908a013220ac90068b483103950f5521e
RealPinkRabbit <sdg0896@gmail.com>
2023-11-04 17:32:12 +0900

    스톤의 운동 범위 설정
    x/y_min/max_boundary 변수로 초기화 가능

--- stone.py
+++ stone.py
@@ -1,13 +1,19 @@
 from pico2d import *
 from math import *
 
+# 추후 조정
+x_min_boundary = 0
+x_max_boundary = 1280
+y_min_boundary = 0
+y_max_boundary = 800
+
 class blue_stone:
     image = None
     minV = 0.02
     vDecRate = 0.98
-    def __init__(self, x = 600, y = 750):
+    def __init__(self, x = 600, y = 500):
         self.x, self.y = x, y
-        self.vx, self.vy = 100, -100
+        self.vx, self.vy = 300, -300
         self.radius = 32
         if (blue_stone.image == None):
             blue_stone.image = load_image('Stone_Blue_64x64.png')
@@ -17,22 +23,22 @@ class blue_stone:
 
     def update(self):
         self.x += self.vx
-        if (self.x < 0 + self.radius):
-            self.x += 2*(self.radius - self.x)
+        if (self.x < x_min_boundary + self.radius):
+            self.x += 2*(self.radius - (self.x - x_min_boundary))
             self.vx *= -1
-        elif (self.x > 1280 - self.radius):
-            self.x -= 2*(self.x - (1280 - self.radius))
+        elif (self.x > x_max_boundary - self.radius):
+            self.x -= 2*(self.x - (x_max_boundary - self.radius))
             self.vx *= -1
         self.vx *= blue_stone.vDecRate
         if (fabs(self.vx) < blue_stone.minV):
             self.vx = 0
 
         self.y += self.vy
-        if (self.y < 0 + self.radius):
-            self.y += 2*(self.radius - self.y)
+        if (self.y < y_min_boundary + self.radius):
+            self.y += 2*(self.radius - (self.y - y_min_boundary))
             self.vy *= -1
-        elif (self.y > 800 - self.radius):
-            self.y -= 2*(self.y - (800 - self.radius))
+        elif (self.y > y_max_boundary - self.radius):
+            self.y -= 2*(self.y - (y_max_boundary - self.radius))
             self.vy *= -1
         self.vy *= blue_stone.vDecRate
         if (fabs(self.vx) < blue_stone.minV):
