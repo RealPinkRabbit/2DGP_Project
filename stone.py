c6052a91b9f644c0b413479f98dac962edabb99c
DongGeunShin <sdg0896@gmail.com>
2023-11-08 00:52:36 +0900

    두 stone의 충돌지점을 구하는 메서드 작성 (올바로 동작하는지는 모름...)

--- stone.py
+++ stone.py
@@ -2,17 +2,57 @@ from pico2d import *
 from math import fabs, pow, sqrt, acos
 
 
-# 해당 벡터의 크기가 1인 벡터를 반환하는 메서드
+# 해당 벡터의 단위벡터를 반환하는 메서드
 def get_unit_vector_xy(vx, vy):
     div = sqrt(pow(vx, 2) + pow(vy, 2))
+    if div == 0:
+        return 0, 0
     return vx / div, vy / div
 
-# o에 대해 상대적인 a의 벡터(충돌지점)을 반환하는 메서드
+# 스톤 o에 대하여 스톤 a의 상대적인 벡터(충돌지점)를 반환하는 메서드
 def get_relative_collision_xy(avx, avy, ovx, ovy):
     rvx, rvy = ovx-avx, ovy-avy
     rvx *= -1
     rvy *= -1
-    return get_unit_vector_xy(rvx, rvy)
+    return blue_stone.default_radius * get_unit_vector_xy(rvx, rvy)
+
+# 두 스톤의 충돌지점의 좌표를 구하는 메서드
+def get_collision_xy(ax, ay, avx, avy, ox, oy, ovx, ovy):
+    a_relative_vx, a_relative_vy = get_relative_collision_xy(avx, avy, ovx, ovy)
+    o_relative_vx, o_relative_vy = -a_relative_vx, -a_relative_vy
+
+    moved_ax, moved_ay = ax + a_relative_vx, ay + a_relative_vy
+    moved_ox, moved_oy = ox + o_relative_vx, oy + o_relative_vy
+    cx, cy = get_cross_xy(moved_ax, moved_ay, avx, avy, moved_ox, moved_oy, ovx, ovy)
+    return cx, cy
+
+# 두 점의 좌표와 방향을 알 때, 두 직선의 교점을 구하는 메서드
+# 단, 기울기가 같은 입력은 받지 않도록 함
+# round() 메서드로 약간 깎아냄
+def get_cross_xy(ax, ay, avx, avy, ox, oy, ovx, ovy):
+    aux, auy = get_unit_vector_xy(avx, avy)
+    oux, ouy = get_unit_vector_xy(ovx, ovy)
+    if (round(abs(aux), 4) == round(abs(oux), 4) & aux * oux >= 0):
+        print("get_cross_xy exception occured")
+        return None, None
+    else: # 기울기가 다르면
+        if (round(avx,4) == 0.0000): # a의 운동이 y축과 평행하면
+            om = ovy/ovx
+            cx = ay
+            cy = om(ay - ox) + oy
+            return cx, cy
+        elif (round(ovx,4) == 0.0000): # o의 운동이 y축과 평행하면
+            am = avy/avx
+            cx = oy
+            cy = am(oy - ax) + ay
+            return cx, cy
+        else: # 두 운동 모두 y축과 평행하지 않으면
+            am = avy/avx
+            om = ovy/ovx
+            cx = (ax * am - ay - ox * om + oy) / (am - om)
+            cy = am * (cx - ax) + ay
+            return cx, cy
+
 
 # 추후 조정
 x_min_boundary = 0
@@ -26,11 +66,12 @@ class blue_stone:
     image = None
     minV = 0.02
     vDecRate = 0.98
+    default_radius = 32
 
     def __init__(self, x = 100, y = 100):
         self.x, self.y = x, y
         self.vx, self.vy = 30, 30
-        self.radius = 32
+        self.radius = blue_stone.default_radius
         if (blue_stone.image == None):
             blue_stone.image = load_image('Stone_Blue_64x64.png')
 
