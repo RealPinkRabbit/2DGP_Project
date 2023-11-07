18cdb0e72937947cbadbe7053f6967d2ba31036e
DongGeunShin <sdg0896@gmail.com>
2023-11-07 17:39:28 +0900

    두 stone의 충돌여부를 반환하는 collide 함수 작성

--- game_world.py
+++ game_world.py
@@ -1,5 +1,6 @@
 # 모든 화면 상에 그려지는 객체들은
 # update()함수, draw()함수를 꼭 가져야 함
+from math import pow
 
 objects = [ [] for _ in range(10) ]
 
@@ -21,3 +22,11 @@ def render_object():
     for layer in objects:
         for o in layer:
             o.draw()
+
+def collide(a, b):
+    a_x, a_y, a_rad = a.get_bc()
+    b_x, b_y, b_rad = b.get_bc()
+
+    if (pow(a_x-b_x)+pow(a_y-b_y) > a_rad+b_rad):
+        return False
+    return True
