a5c6da1c8b8f3ba9ad99e4dbf57445f525e7e138
RealPinkRabbit <sdg0896@gmail.com>
2023-11-12 03:30:30 +0900

    로고모드에서 바로 플레이모드로 전환

--- logo_mode.py
+++ logo_mode.py
@@ -1,5 +1,6 @@
 from pico2d import get_events, load_image, clear_canvas, update_canvas, get_time
 import game_framework
+import play_mode
 import title_mode
 
 
@@ -7,7 +8,7 @@ def init():
     global image
     global logo_start_time
 
-    image = load_image('tuk_credit.png')
+    image = load_image('tuk_credit_1280x800.png')
     logo_start_time = get_time()
 
 
@@ -16,12 +17,12 @@ def finish():
 
 def update():
     if get_time() - logo_start_time >= 2.0:
-        game_framework.change_mode(title_mode)
+        game_framework.change_mode(play_mode)
     pass
 
 def draw():
     clear_canvas()
-    image.draw(400, 300)
+    image.draw(640, 400)
     update_canvas()
     pass
 
